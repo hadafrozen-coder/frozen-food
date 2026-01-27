f76d84e3347392b91c1504ea67f360623cd8cd16
Bonang Skaton <bonang1skaton@gmail.com>
2026-01-28 01:09:21 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+document.querySelectorAll('.btnDetail').forEach(item=>{
+item.addEventListener('click',(e) => {
+  let parent = e.target.parentNode.parentNode;
+
+  let gambar = parent.querySelector('.card-img-top').src;
+  let harga = parent.querySelector('.harga').innerHTML;
+  let judul = parent.querySelector('.card-text').innerHTML;
+  let deskripsi = parent.querySelector('.deskripsi') ? parent.querySelector('.deskripsi').innerHTML:'<i>tidak tersedia</i>';
+
+  //let deskripsi = parent.querySelector('.deskripsi') ? parent.querySelector('.deskripsi').innerHTML:'<i>tidak tersedia</i>'; { fungsi script yang dicomand :jika barang tidak ada maka akan menampilkan "tidak tersedia"}
+
+  let tombolModal = document.querySelector(`.btnModal`);
+   tombolModal.click(); 
+
+   document.querySelector('.modalTitle').innerHTML = judul;
+   let image = document.createElement('img');
+   image.src = gambar;
+   image.classList.add('w-100');
+   document.querySelector('.modalImage').innerHTML ='';
+   document.querySelector('.modalImage').appendChild(image);
+   document.querySelector('.modalDeskripsi').innerHTML = deskripsi;
+   document.querySelector('.modalHarga').innerHTML = harga;
+
+   const nohp = '+6285692928708';
+   let pesan = `https://api.whatsapp.com/send?phone=${+6285692928708}&text=hai kak,saya mau pesan product ini ${gambar}, dengan harga ${harga}`;
+
+
+    document.querySelector('.btnBeli').href = pesan;
+
+});
+});
+// 1:30:00
